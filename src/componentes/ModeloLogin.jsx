6f4d46f63c0a4653e5ff25381a7dd3013f8ca546
GFernandoCT <ferna.torres.correa@gmail.com>
2021-06-02 20:01:46 +0200

    Parte 1 del proyecto Acabada

--- src/componentes/ModeloLogin.jsx
+++ src/componentes/ModeloLogin.jsx
@@ -4,11 +4,26 @@ import axios from 'axios';
 const LoginFormulario = () => {
     const [usuario,setUsuario] = useState('');
     const [contraseña,setContraseña] = useState('');
+    const [errores,setError] = useState('');
 
-    const subirManejado = (e) => {
+    const subirManejado = async (e) => {
         e.preventDefault();
 
-        const authObject = { 'Project-ID': "aaed14ce-2955-4169-99f9-21adb93dbd68", 'User-Name': username, 'User-Secret': password };
+        const authObject = { 'Project-ID': "aaed14ce-2955-4169-99f9-21adb93dbd68", 'User-Name': usuario, 'User-Secret': contraseña };
+
+        try {
+
+            // usuario | contraseña => chatengine -> give messages
+            await axios.get('https://api.chatengine.io/chats',{ headers: authObject });
+            
+            localStorage.setItem('username', usuario);
+            localStorage.setItem('password', contraseña);
+
+            window.location.reload();
+            setError('');
+        } catch (error) {
+            setError('Los datos no son correctos, intentalo de nuevo.');
+        }
 
         // usuario / contraseña => chatEngine => devolver inicio
         // correcto => logea dentro
@@ -21,14 +36,17 @@ const LoginFormulario = () => {
                 <h1 className="title">Chit Chat</h1>
                 <form onSubmit={subirManejado}>
                     <input type="text" value={usuario} onChange={(e) => setUsuario(e.target.value)} className="input" placeholder="Usuario" required />
-                    <input type="text" value={contraseña} onChange={(e) => setContraseña(e.target.value)} className="input" placeholder="Contraseña" required />
+                    <input type="password" value={contraseña} onChange={(e) => setContraseña(e.target.value)} className="input" placeholder="Contraseña" required />
                     <div align="center">
                         <button type="submit" className="button"> 
                             <span>Bienvenido!</span>
                         </button>
                     </div>
                 </form>
+                <h2 className="error">{errores}</h2>
             </div>
         </div>
     );
 }
+
+export default LoginFormulario;
